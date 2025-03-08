9d52238a1b8c7188ed8a06af9d7d98a264faf784
Matheus Freire <matheusfreire.cbcc@gmail.com>
2025-03-07 22:56:43 -0300

    creating a html page with top rated movies from tmdb

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,20 @@
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.util.Comparator;
 import java.util.List;
 
 import model.Movie;
 import util.YamlReader;
+import view.HtmlGenerator;
 
 public class Main {
     public static void main(String[] args) {
-
-        String url = "https://api.themoviedb.org/3/search/movie?query=matrix&include_adult=false&language=en-US&page=1";
+        String url = "https://api.themoviedb.org/3/movie/top_rated?language=en-US&page=1";
 
         String apiKey = YamlReader.getProperties("src/resources/properties.yml", "apiToken");
 
@@ -23,13 +27,25 @@ public class Main {
                 .GET()
                 .build();
 
+        List<Movie> movies;
+
         try {
             HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
-            System.out.println("Resposta da API: " + response.body());
-            List<Movie> movies = MovieConverter.parseMovie(response.body());
+            if(response.statusCode() != 200)
+                throw new RuntimeException(response.body());
+            System.out.println(response.body());
+            movies = MovieConverter.parseMovie(response.body());
             System.out.println(movies.size() + " filmes encontrados!");
         } catch (Exception e) {
             throw new RuntimeException("Erro ao obter os dados do API: " + e.getMessage());
+        }
+
+        try (PrintWriter writer = new PrintWriter(new FileWriter("filmes.html"))) {
+            HtmlGenerator generator = new HtmlGenerator(writer);
+            movies = movies.stream().sorted(Comparator.comparing(Movie::getVote_average).reversed()).toList();
+            generator.generate(movies);
+        } catch (IOException e) {
+            throw new RuntimeException("Erro ao gerar HTML: " + e.getMessage());
         }
     }
 }
---
+++ src/view/HtmlGenerator.java
@@ -0,0 +1,56 @@
+package view;
+
+import model.Movie;
+
+import java.io.PrintWriter;
+import java.util.List;
+import java.util.Optional;
+
+public class HtmlGenerator {
+    private final PrintWriter writer;
+
+    private static final String POSTER_BASE_URL = "https://image.tmdb.org/t/p/w500";
+
+    public HtmlGenerator (PrintWriter writer) {
+        this.writer = writer;
+    }
+
+    public void generate(List<Movie> movies) {
+        writer.write(getHeader());
+        writer.write("<body><div class='container'><div class='row'>");
+        movies.forEach(movie -> writer.write(getMovieCard(movie)));
+        writer.write("</div></div></body></html>");
+        writer.flush();
+    }
+
+    private String getHeader() {
+        return
+                """
+                <!DOCTYPE html>
+                <html lang="en">
+                <head>
+                    <meta charset=\"utf-8\">
+                    <meta name=\"viewport\" content=\"width=device-width, initial-scale=1, shrink-to-fit=no\">
+                    <link rel=\"stylesheet\" href=\"https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css\" 
+                        + "integrity=\"sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm\" crossorigin=\"anonymous\">					
+                </head>
+                """;
+    }
+
+    private String getMovieCard(Movie movie) {
+
+        String year = Optional.ofNullable(movie.getRelease_date())
+                .map(date -> String.valueOf(date.getYear()))
+                .orElse("Ano não disponível");
+        return
+                """
+                <div class=\"card text-white bg-dark mb-3\" style=\"max-width: 18rem;\">
+                    <h4 class=\"card-header\">%s</h4>
+                    <div class=\"card-body\">
+                        <img class=\"card-img\" src=\"%s\" alt=\"%s\">
+                        <p class=\"card-text mt-2\">Nota: %s - Ano: %s</p>
+                    </div>
+                </div>
+                """.formatted(movie.getTitle(), POSTER_BASE_URL + movie.getPoster_path(), movie.getTitle(), movie.getVote_average(), year);
+    }
+}
